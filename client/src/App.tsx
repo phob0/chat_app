83c3957c5c3601d1e6b35f2bba7fdda7b76c677c
phob0 <ph0bo@protonmail.com>
2023-03-23 01:27:17 +0200

    client material ui scratch

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import AppBar from './AppDrawerBar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+export default function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <AppBar />
+    </ThemeProvider>
+  );
+}
